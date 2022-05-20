3c0cea9d41fe0578840dc975aba5aa2bf23a0d58
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-05-20 20:23:36 +0600

    update layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import Head from 'next/head';
-import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import React from "react";
+import Head from "next/head";
+import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
 
 export default function Layout({ children }) {
-	return (
-		<div>
-			<Head>
-				<title>Next E-com</title>
-			</Head>
-			<AppBar position="static">
-				<Toolbar>
-					<Typography>Next E-com</Typography>
-				</Toolbar>
-			</AppBar>
-			<Container>{children}</Container>
-            <footer>
-                <Typography>
-                    @All right reserved. Next E-com.
-                </Typography>
-            </footer>
-		</div>
-	);
+  return (
+    <div>
+      <Head>
+        <title>Next E-com</title>
+      </Head>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>Next E-com</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>{children}</Container>
+      <footer>
+        <Typography>@All right reserved. Next E-com 2022.</Typography>
+      </footer>
+    </div>
+  );
 }
